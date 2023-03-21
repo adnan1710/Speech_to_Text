5aa24189c6e9349a33522609d76373663e4c853e
Md. Adnan Akhtar <adnanakhtar1710@gmail.com>
2023-03-22 03:34:35 +0530

    Fixed an issue where start button was not appearing after being idle

--- script.js
+++ script.js
@@ -1,41 +1,53 @@
-const speechRecognition = window.webkitSpeechRecognition
-const recognition = new speechRecognition()
-const textbox = document.getElementById("textbox")
-const instructions = document.getElementById("instructions")
-const startBtn = document.getElementById("start-btn")
-const stopBtn = document.getElementById("stop-btn")
+const speechRecognition = window.webkitSpeechRecognition;
+const recognition = new speechRecognition();
+const textbox = document.getElementById("textbox");
+const instructions = document.getElementById("instructions");
+const startBtn = document.getElementById("start-btn");
+const stopBtn = document.getElementById("stop-btn");
 
-let content = ''
+let content = '';
 
-recognition.continuous = false
+recognition.continuous = false;
+recognition.interimResults = true;
 
 const startRecognition = () => {
-    recognition.start()
-    startBtn.style.display = "none"
-    stopBtn.style.display = "block"
+
+    textbox.value = ""
+    recognition.start();
+    startBtn.style.display = "none";
+    stopBtn.style.display = "block";
 };
 
 const stopRecognition = () => {
-    recognition.stop()
-    stopBtn.style.display = "none"
-    startBtn.style.display = "block"
-    instructions.textContent = "Press Start"
+
+    recognition.stop();
+    stopBtn.style.display = "none";
+    startBtn.style.display = "block";
+    instructions.textContent = "Press Start";
 };
 
 const updateContent = () => content = textbox.value;
 
-recognition.onstart = () => instructions.textContent = "Listening"
-recognition.onspeechend = () => instructions.textContent = "Can't hear you. No Activity"
+
+recognition.onstart = () => instructions.textContent = "Listening";
+
+recognition.onspeechend = () => {
+
+    instructions.textContent = "No Activity";
+    stopBtn.style.display = "none";
+    startBtn.style.display = "block";
+}
+
 recognition.onerror = () => {
-    instructions.textContent = "Error Occured"
+    instructions.textContent = "Error Occured";
     stopRecognition();
 }
 
 recognition.onresult = (event) => {
 
     const current = event.resultIndex;
-    const transcript = event.results[current][0].transcript
-    content += transcript
+    const transcript = event.results[current][0].transcript;
+    content += transcript;
     textbox.value = content;
 }
 
